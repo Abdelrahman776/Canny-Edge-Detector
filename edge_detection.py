cb6c32747c84f0bb25a43d14c01764e736366e12
Abdelrahman kamal <abdelrahmannkamal@gmail.com>
2024-04-05 10:41:06 +0200

    fix issues

--- edge_detection.py
+++ edge_detection.py
@@ -2,55 +2,101 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-def canny_edge_detection():
-  img_path = "D:\oneDrive\Desktop\Canny-Edge-Detector\static\images\peppers.png"
-  img_rgb = preprocess_image(img_path)
-  magnitude, angle = calculate_gradient(img_rgb)
-  angle = round_angles(angle)
-  edges = non_maxima_suppression(img_rgb)
-  edges = double_thresholding(edges)
-  edges = check_neighbors(edges)
-  processed_image_path = save_image(edges)
-  plt.imshow(edges, cmap='gray')
-  plt.axis('off')
-#   plt.show()
-  return processed_image_path
+def canny_edge_detection( image ,lower=-1, upper=-1):
+    # img_path = r"D:/oneDrive/Desktop/Canny-Edge-Detector/static/images/Pyramids2.jpg"  # Using raw string or forward slashes
+    # img = cv2.imread(img_path)  
+    nparr = np.fromstring(image, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    img = preprocess_image(img)
+    magnitude, angle = gradient_magnitude_angle(img)
+    edges = non_maxima_suppression(magnitude, angle)
+    if lower==-1 and upper==-1:
+        edges = double_thresholding(edges, low=0.2*np.max(edges), high= 0.7*np.max(edges))
+    else:
+        edges = double_thresholding(edges, low=lower, high= upper)
+    edges = check_neighbors(edges)
+    plt.imshow(edges, cmap='gray')
+    plt.axis('off')
+    plt.show()
+    return edges
 
-def preprocess_image(img_path):
-    img = cv2.imread(img_path)
+def gaussian_kernel(size, sigma=1):
+    kernel = np.fromfunction(lambda x, y: (1 / (2 * np.pi * sigma ** 2)) * np.exp(-((x - (size - 1) ** 2 + (y - (size - 1) ** 2)) / (2 * sigma ** 2))), (size, size))
+    return kernel / np.sum(kernel)
 
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img_rgb = cv2.GaussianBlur(img_rgb, (5, 5), 1)
-    return img_rgb
+def preprocess_image(img):
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    rows = img.shape[0]
+    cols = img.shape[1]
+    size = 5
+    sigma = 1
+    gaussian_filter = gaussian_kernel(size, sigma)
+    img = np.pad(img, (size-1, size-1), mode='constant')
+    gaussian_img = np.zeros((rows, cols))
+    for i in range(rows-size):
+        for j in range(cols-size):
+            gaussian_img[i, j] = np.sum(img[i:i+size, j:j+size] * gaussian_filter) 
+    return gaussian_img
 
-def calculate_gradient(img_rgb):
-    gx = cv2.Sobel(img_rgb, cv2.CV_64F, 1, 0, ksize=3)
-    gy = cv2.Sobel(img_rgb, cv2.CV_64F, 0, 1, ksize=3)
+def gradient_magnitude_angle(img):
+    sobel_kernel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    sobel_kernel_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+    rows = img.shape[0]
+    cols = img.shape[1]
+    size = 3
+    img = np.pad(img, (size-1, size-1), mode='constant')
+    gx = np.zeros((rows, cols))
+    gy = np.zeros((rows, cols))
+    for i in range(rows-size):
+        for j in range(cols-size):
+            gx[i, j] = np.sum(img[i:i+size, j:j+size] * sobel_kernel_x)
+            gy[i, j] = np.sum(img[i:i+size, j:j+size] * sobel_kernel_y)
     magnitude = np.sqrt(gx**2 + gy**2)
-    angle = np.arctan2(gy, gx) * 180 / np.pi
+    angle = np.arctan2(gy, gx) * 180 / np.pi 
+    angle = np.round((angle%360) / 45) * 45 
     return magnitude, angle
 
-def round_angles(angle):
-    angle = np.round(angle / 45) * 45
-    return angle
-
-def non_maxima_suppression(img_rgb):
-    suppressed = cv2.Canny(img_rgb, 100, 200)
+def non_maxima_suppression(magnitude, angle):
+    suppressed = np.zeros(magnitude.shape)
+    for i in range(1, magnitude.shape[0] - 1):
+        for j in range(1, magnitude.shape[1] - 1):
+            if angle[i, j] == 0 or angle[i, j] == 180 :
+                if magnitude[i, j] == max(magnitude[i, j], magnitude[i, j+1], magnitude[i, j-1]):
+                    suppressed[i, j] = magnitude[i, j]
+            elif angle[i, j] == 45 or angle[i, j] == 225:
+                if magnitude[i, j] == max(magnitude[i, j], magnitude[i+1, j-1], magnitude[i-1, j+1]):
+                    suppressed[i, j] = magnitude[i, j]
+            elif angle[i, j] == 90 or angle[i, j] == 270:
+                if magnitude[i, j] == max(magnitude[i, j], magnitude[i+1, j], magnitude[i-1, j]):
+                    suppressed[i, j] = magnitude[i, j]
+            elif angle[i, j] == 135 or angle[i, j] == 315:
+                if magnitude[i, j] == max(magnitude[i, j], magnitude[i+1, j+1], magnitude[i-1, j-1]):
+                    suppressed[i, j] = magnitude[i, j]
     return suppressed
 
-def double_thresholding(edges):
-    edges[edges >= 100] = 255
-    return edges
+def double_thresholding(img:np.ndarray, lower_threshold:float, upper_threshold:float):
+    [n,m] = img.shape
+    marking = np.zeros(img.shape)
+    for x in range(n):
+        for y in range(m):
+            if img[x,y] > upper_threshold:
+                marking[x,y] = 255
+            elif img[x,y] < lower_threshold:
+                marking[x,y] = 0
+            else:
+                marking[x,y] = 25
+    return marking
 
 def check_neighbors(edges):
-    for i in range(1, edges.shape[0] - 1):
-        for j in range(1, edges.shape[1] - 1):
-            if edges[i, j] == 255:
-                if (edges[i-1:i+2, j-1:j+2] == 255).sum() < 2:
-                    edges[i, j] = 0
+    [rows, cols] = edges.shape
+    for x in range(rows-1):
+        for y in range(cols-1):
+            if edges[x,y] == 25:
+                if ((edges[x+1,y] == 255) or (edges[x-1,y] == 255) or (edges[x+1,y+1] == 255) or (edges[x+1,y-1] == 255) or
+                (edges[x-1,y+1] == 255) or (edges[x-1,y-1] == 255) or (edges[x,y+1] == 255) or (edges[x,y-1] == 255)):
+                    edges[x,y] = 255
+                else:
+                    edges[x,y] = 0
     return edges
 
-def save_image(edges):
-    return cv2.imwrite("D:\oneDrive\Desktop\Canny-Edge-Detector\static\images\processed_image.png", edges)
 
-canny_edge_detection()    
--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 from flask import Flask,render_template,request,redirect,url_for
-import os
+import cv2
+import numpy as np
+import base64
 from edge_detection import canny_edge_detection
-from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['POST','GET'])
 def index():
-    return render_template("index.html")
-
-# @app.route('/', methods=['POST'])
-# def upload_and_process():
-#     if 'og' not in request.files:
-#         return "No file part"
-
-#     og = request.files['og']
-
-#     if og.filename == '':
-#         return "No selected file"
-
-#     processed_image_path = "path/to/processed/image.jpg"
-
-#     # Display the processed image or any message in the template
-#     return render_template('index.html', ogname=og.filename)
+    if request.method == 'GET':
+        return render_template("index.html")
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        if file.filename == '':
+            return 'No selected file'
+        if file:
+            # Read the image
+            img_bytes = file.read()
+            # Convert to base64
+            encoded_img = base64.b64encode(img_bytes).decode('utf-8')
+            #  filter
+            filtered_image = canny_edge_detection(encoded_img,50, 100)
+            return render_template('index.html', original_image=encoded_img, filtered_image=filtered_image)
 
 
 if __name__ == '__main__':
